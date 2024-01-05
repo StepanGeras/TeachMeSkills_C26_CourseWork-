ac787b9971bb44a5614b352a970013074334af2f
Shifernik <m.Disai@mail.ru>
2024-01-05 19:46:36 +0300

    First commit
    Курсовая работа
    
    что у меня делает программа
    1. Запрашивает логин и пароль (логин и пароль зашифрованы)
    2. Дальше просит путь к папке (добавлю еще проверку на правильность пути)
    3. После получения пути к папке идет сортировка на валидность файлов и невалидные закидываются в отдельную папку
    4. Написал логер, но не добавлял его
    Осталось сделать статистику, исключения дописать, сессию сделать

--- src/exception/WrongLoginException.java
+++ src/exception/WrongLoginException.java
@@ -4,10 +4,6 @@ public class WrongLoginException extends Exception {
 
     private int validationExceptionCode;
 
-    public WrongLoginException() {
-        super();
-    }
-
     public WrongLoginException(int validationExceptionCode, String login) {
         super(login);
         this.validationExceptionCode = validationExceptionCode;
